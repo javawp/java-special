16d8019619edf30f420e9cff95c0d047ef1d0b98
王鹏 <javawp@icloud.com>
2016-07-01 13:26:17 +0800

    模拟 java.lang.StackOverflowError

--- java-special/src/chapter03/oom/StackOverFlowTest.java
+++ java-special/src/chapter03/oom/StackOverFlowTest.java
@@ -2,11 +2,31 @@ package chapter03.oom;
 
 public class StackOverFlowTest {
 
-	public static void main(String []args) {
+	public static void main(String[] args) {
+		new StackOverFlowTest().testStackOver(1);
 		new StackOverFlowTest().testStackOver();
 	}
-	
-	public void testStackOver() {
+
+	/**
+	 * 死递归 
+	 *
+	 * @作者: 王鹏
+	 * @创建时间: 2016年7月1日-上午11:45:32
+	 */
+	private void testStackOver() {
 		testStackOver();
+	}
+	
+	/**
+	 * 通过控制递归层数
+	 * @param i
+	 *
+	 * @作者: 王鹏
+	 * @创建时间: 2016年7月1日-上午11:45:01
+	 */
+	public void testStackOver(int i) {
+		if(i++ == 1) System.out.println("i++ 与 数字比较, 先比较, 再累加.");
+		if (i++ > 200) return;
+		testStackOver(i);
 	}
 }
