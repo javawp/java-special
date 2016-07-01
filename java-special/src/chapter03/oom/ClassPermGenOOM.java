2f9e97118063a93c1e30f255e1801be465a26afc
王鹏 <javawp@icloud.com>
2016-07-01 10:16:27 +0800

    CGlib不断创建子类让Perm区域内存溢出

--- java-special/src/chapter03/oom/ClassPermGenOOM.java
+++ java-special/src/chapter03/oom/ClassPermGenOOM.java
@@ -1,30 +1,40 @@
 package chapter03.oom;
 
 import java.lang.reflect.Method;
+import java.net.URLClassLoader;
 
 import net.sf.cglib.proxy.Enhancer;
 import net.sf.cglib.proxy.MethodInterceptor;
 import net.sf.cglib.proxy.MethodProxy;
 
+/**
+ * @描述: CGlib不断创建子类让Perm区域内存溢出
+ * 
+ * -XX:+PrintGCDetails -XX:PermSize=10m -XX:MaxPermSize=10m -XX:+TraceClassUnloading
+ * 
+ * @作者: 王鹏
+ * @创建时间: 2016年7月1日-上午9:40:59
+ * @版本: 1.0
+ */
 public class ClassPermGenOOM {
 
-	/*private static URLClassLoader parentClassLoader;
+	private static URLClassLoader parentClassLoader;
 
 	static {
-		parentClassLoader = (URLClassLoader) ClassPermGenOOM.class
-				.getClassLoader();
-	}*/
+		parentClassLoader = (URLClassLoader) ClassPermGenOOM.class.getClassLoader();
+	}
 
-	public static Object createProxy(Class <?>targetClass) {
+	public static Object createProxy(Class<?> targetClass) {
 		Enhancer enhancer = new Enhancer();
 		enhancer.setSuperclass(targetClass);
-		//enhancer.setClassLoader(parentClassLoader);
+		enhancer.setClassLoader(new URLClassLoader(parentClassLoader.getURLs(), parentClassLoader));
 		enhancer.setUseCache(false);
 		enhancer.setCallback(new MethodInterceptor() {
-			public Object intercept(Object object, Method method, Object[] args, MethodProxy methodProxy) throws Throwable {
+			@Override
+			public Object intercept(Object object, Method method, Object[] args,
+					MethodProxy methodProxy) throws Throwable {
 				return methodProxy.invokeSuper(object, args);
-			}
-		});
+			}});
 		return enhancer.create();
 	}
 
--- java-special/src/chapter03/oom/StringInternPermGenOOM.java
+++ java-special/src/chapter03/oom/StringInternPermGenOOM.java
@@ -8,7 +8,7 @@ public class StringInternPermGenOOM {
 	/**
 	 * 请在命令行下运行
 	 * 运行方式: -XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+UseParallelOldGC
-	 * 运行方式：java -XX:PermSize10m -XX:MaxPermSize=10m chapter3.fullgc.StringInternPermGenFullGC
+	 * 运行方式：java -XX:PermSize=2m -XX:MaxPermSize=2m chapter03.oom.StringInternPermGenOOM
 	 * 
 	 * @param args
 	 */
