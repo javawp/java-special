dedc371b72302b2159874f8cccb8cd7db7786d56
王鹏 <javawp@icloud.com>
2016-07-01 11:35:30 +0800

    DirectBuffer 直接内存操作

--- java-special/src/chapter03/oom/ByteBufferOOM.java
+++ java-special/src/chapter03/oom/ByteBufferOOM.java
@@ -2,13 +2,20 @@ package chapter03.oom;
 
 import java.nio.ByteBuffer;
 
+import sun.nio.ch.DirectBuffer;
+
 public class ByteBufferOOM {
 
-	//-XX:MaxDirectMemorySize=256m
-	public static void main(String []args) {
-		ByteBuffer.allocateDirect(257 * 1024 * 1024);
-		//DirectBuffer byteBuffer = (DirectBuffer)ByteBuffer.allocateDirect(256 * 1024 * 1024);
-		//((DirectBuffer)byteBuffer).cleaner().clean();
-		//ByteBuffer.allocateDirect(1024 * 1024);
+	// -XX:MaxDirectMemorySize=256m
+	public static void main(String[] args) {
+		// ByteBuffer.allocateDirect(257 * 1024 * 1024);
+		
+//		ByteBuffer.allocateDirect(256 * 1024 * 1024);
+//		ByteBuffer.allocateDirect(1024 * 1024);
+		
+		DirectBuffer byteBuffer = (DirectBuffer) ByteBuffer.allocateDirect(256 * 1024 * 1024);
+//		byteBuffer = null;
+		((DirectBuffer) byteBuffer).cleaner().clean();
+		ByteBuffer.allocateDirect(1024 * 1024);
 	}
 }
