30fd7a7a75da728f30319bc80b02d896f13e3c12
王鹏 <javawp@icloud.com>
2016-07-17 10:05:58 +0800

    格式化代码, CompletionService

--- java-special/src/chapter05/tools/CompletionServiceTest.java
+++ java-special/src/chapter05/tools/CompletionServiceTest.java
@@ -11,11 +11,11 @@ import java.util.concurrent.Future;
 
 public class CompletionServiceTest {
 
-	public static void main(String []args) throws InterruptedException, ExecutionException {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		final Random random = new Random();
 		ExecutorService executorService = Executors.newFixedThreadPool(10);
-		CompletionService<String>completionService = new ExecutorCompletionService<String>(executorService);
-		for(int i = 0 ; i < 100 ; i++) {
+		CompletionService<String> completionService = new ExecutorCompletionService<String>(executorService);
+		for (int i = 0; i < 100; i++) {
 			final int num = i;
 			completionService.submit(new Callable<String>() {
 				public String call() {
@@ -28,7 +28,7 @@ public class CompletionServiceTest {
 				}
 			});
 		}
-		for(int i = 0 ; i < 100 ; i++) {
+		for (int i = 0; i < 100; i++) {
 			Future<String> f = completionService.take();
 			System.out.println(f.get());
 		}
